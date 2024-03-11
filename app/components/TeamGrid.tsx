6ac06926dcc435de48c24b82adf85584ef2134aa
Monarch <92059211+Monarch47@users.noreply.github.com>
2024-03-11 23:23:41 +0530

    fewfwe

--- app/components/TeamGrid.tsx
+++ app/components/TeamGrid.tsx
@@ -3,16 +3,15 @@ import { TeamGridData } from "@/data";
 
 const renderTeamGridData = TeamGridData.map((teamMember, index) => {
   return (
-    <div
-      className="flex flex-col items-center justify-center gap-4"
-      key={index}
-    >
-      <img src={teamMember.path} className="w-40 aspect-auto rounded-full" />
-      <div className="flex flex-col gap-2">
-        <h2>{teamMember.name}</h2>
-        <h3>{teamMember.role}</h3>
+    <React.Fragment key={index}>
+      <div className="flex flex-col items-center justify-center gap-4">
+        <img src={teamMember.path} className="w-40 aspect-auto rounded-full" />
+        <div className="flex flex-col gap-2">
+          <h2>{teamMember.name}</h2>
+          <h3>{teamMember.role}</h3>
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 });
 
